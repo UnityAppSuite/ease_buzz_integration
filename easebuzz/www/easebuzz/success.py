493e5d4f85f2c9dc6cbdc37f1a44cb23194408fe
vikaswalnutedu <vikas@walnutedu.in>
2023-07-19 08:34:10 +0530

    fix: success page

--- easebuzz/www/easebuzz/success.py
+++ easebuzz/www/easebuzz/success.py
@@ -2,7 +2,12 @@ import frappe
 
 
 def get_context(context):
-    frappe.local.login_manager.login_as("Administrator")
-    data = frappe.form_dict
-    frappe.get_doc("EaseBuzz Settings").handle_response(data)
-    frappe.local.login_manager.login_as("Guest")
+    if frappe.session.user == "Guest":
+        frappe.local.login_manager.login_as("Administrator")
+        data = frappe.form_dict
+        frappe.get_doc("EaseBuzz Settings").handle_response(data)
+        frappe.local.login_manager.login_as("Guest")
+    else:
+        data = frappe.form_dict
+        frappe.get_doc("EaseBuzz Settings").handle_response(data)
+        
