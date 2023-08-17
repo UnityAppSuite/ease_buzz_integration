1d37d8aa0ea9572e33f7907c7c1ac127a5ba8e5f
vikaswalnutedu <vikas@walnutedu.in>
2023-08-17 13:00:48 +0530

    delete: Split Payment doctype

--- easebuzz/easebuzz/doctype/split_payment/split_payment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Hybrowlabs and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class SplitPayment(Document):
-	pass
--- easebuzz/easebuzz/doctype/split_payment/test_split_payment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Hybrowlabs and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSplitPayment(FrappeTestCase):
-	pass
