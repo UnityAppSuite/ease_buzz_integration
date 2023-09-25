9829ee0d1032efe3a612a321b53cb182223c3fd4
vikaswalnutedu <vikas@walnutedu.in>
2023-09-25 12:14:04 +0530

    fix: easebuzz settings

--- easebuzz/www/easebuzz/success.py
+++ easebuzz/www/easebuzz/success.py
@@ -6,11 +6,11 @@ def get_context(context):
         if frappe.session.user == "Guest":
             frappe.local.login_manager.login_as("Administrator")
             data = frappe.form_dict
-            frappe.get_doc("Easebuzz Settings").handle_response(data)
+            frappe.get_last_doc("Easebuzz Settings").handle_response(data)
             frappe.local.login_manager.login_as("Guest")
         else:
             data = frappe.form_dict
-            frappe.get_doc("Easebuzz Settings").handle_response(data)
+            frappe.get_last_doc("Easebuzz Settings").handle_response(data)
     except Exception as e:
         frappe.logger("easebuzz").exception(e)
             
