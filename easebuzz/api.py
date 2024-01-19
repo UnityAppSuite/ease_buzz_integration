06bed0bb6a4f41bb188f8e50a84f2339d8eee1f3
sankarsubramaniankvs <sankarsubramaiankvs@gmail.com>
2024-01-19 11:29:27 +0530

    fix: mgr webhook call

--- easebuzz/api.py
+++ easebuzz/api.py
@@ -1,9 +1,12 @@
 import frappe
+import requests
 
 @frappe.whitelist(allow_guest=True)
 def webhook_handler(**kwargs):
     try:
         data = frappe.parse_json(kwargs)
         frappe.get_last_doc("Easebuzz Settings").handle_response(data)
+        url = "https://fees.walnutedu.in/index.php/payment/easebuzz_webhook_callback"
+        r = requests.post(url, json = data)
     except Exception as e:
         frappe.logger("easebuzz").exception(e)
