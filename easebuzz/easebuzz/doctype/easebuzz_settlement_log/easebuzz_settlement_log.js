eda6553ff32da895101ab9921cb53192efc40c3f
sankarsubramaniankvs <sankarsubramaiankvs@gmail.com>
2024-01-25 19:53:35 +0530

    feat: settlement log

---
+++ easebuzz/easebuzz/doctype/easebuzz_settlement_log/easebuzz_settlement_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Hybrowlabs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Easebuzz Settlement Log', {
+	// refresh: function(frm) {
+
+	// }
+});
